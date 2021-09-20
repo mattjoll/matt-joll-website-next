a4ecceda880a0880ea801efc8831ca48e23add3c
Matthew Joll <dev@mattjoll.nz>
2021-09-20 23:18:48 +1200

    feat(blog): Add global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,10 +6,18 @@ import Footer from '../components/Footer';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div>
-      <NavBar />
-      <Component {...pageProps} />
-      <Footer />
+    <div className="flex flex-col h-full">
+      <div className="sticky top-0">
+        <NavBar />
+      </div>
+
+      <div className="max-w-4xl mx-auto my-6 w-11/12 md:w-3/4">
+        <Component {...pageProps} />
+      </div>
+
+      <div className="mt-auto">
+        <Footer />
+      </div>
     </div>
   );
 }
