d9a625ea0eb1f365e88841d5e906b958108a5b7e
Matthew Joll <dev@mattjoll.nz>
2021-09-20 23:35:48 +1200

    feat(blog): Add blog feed

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,7 +1,31 @@
 import { NextPage } from 'next';
+import { useEffect, useState } from 'react';
+import { IBlogPost } from '../interfaces/i-blog-post';
+import BlogPostPreview from '../components/BlogPostPreview';
 
 const Blog: NextPage = () => {
-  return <div>Blog</div>;
+  const [blogPosts, setBlogPosts] = useState<IBlogPost[]>([]);
+
+  useEffect(() => {
+    const apiUrl = `${'http://localhost:3001'}/blog/posts`;
+
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((res) => setBlogPosts(res.blogPosts));
+  }, []);
+
+  const blogPostElements = blogPosts.map((blogPost: IBlogPost) => (
+    <BlogPostPreview blogPost={blogPost} key={`${blogPost.id}-preview`} />
+  ));
+
+  return (
+    <div>
+      <h1>Blog</h1>
+      <div className="grid auto-rows-auto gap-4 grid-cols-1 lg:grid-cols-2 mt-5">
+        {blogPostElements}
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
