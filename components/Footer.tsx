c3e0cd81543f31324378c4cc9eed83d78f1fb426
Matthew Joll <dev@mattjoll.nz>
2021-09-25 13:09:05 +1200

    feat(blog): Restyle footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,32 +2,34 @@ import { FunctionComponent } from 'react';
 
 const Footer: FunctionComponent = () => {
   return (
-    <footer className="w-max mx-auto py-4">
+    <footer className="flex flex-col items-center w-max mx-auto py-4">
       <span>This site was made using: </span>
-      <a
-        className="px-3 hover:text-green-600"
-        href="https://www.reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        React
-      </a>
-      <a
-        className="px-3 hover:text-green-600"
-        href="https://tailwindcss.com"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        TailwindCSS
-      </a>
-      <a
-        className="px-3 hover:text-green-600"
-        href="https://www.contentful.com"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Contentful
-      </a>
+      <div>
+        <a
+          className="px-3 hover:text-green-600"
+          href="https://www.reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          React
+        </a>
+        <a
+          className="px-3 hover:text-green-600"
+          href="https://tailwindcss.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          TailwindCSS
+        </a>
+        <a
+          className="px-3 hover:text-green-600"
+          href="https://www.contentful.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Contentful
+        </a>
+      </div>
     </footer>
   );
 };
