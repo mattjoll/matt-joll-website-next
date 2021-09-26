18d4e3de2e4b9ce75e7d2ff954c2b47599fc06e5
Matthew Joll <dev@mattjoll.nz>
2021-09-27 09:56:27 +1300

    ci: Map correct response when loading all blogposts

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -24,12 +24,12 @@ const Blog: NextPage<BlogProps> = (props) => {
 export default Blog;
 
 export const getStaticProps: GetStaticProps<BlogProps> = async () => {
-  const blogPosts: IBlogPost[] = await fetch(
+  const response = await fetch(
     `${process.env.NEXT_PUBLIC_SERVER_URL}/api/blog/posts`
   ).then((res) => res.json());
 
   return {
-    props: { blogPosts },
+    props: { blogPosts: response.blogPosts },
     revalidate: 60,
   };
 };
