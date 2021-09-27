7d924c9696fa1458d8ee7390bea8644468ea4d8e
Matthew Joll <dev@mattjoll.nz>
2021-09-27 13:30:59 +1300

    feat(seo): Add meta tags for the blog landing page

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,6 +1,7 @@
 import { GetStaticProps, NextPage } from 'next';
 import { IBlogPost } from '../../interfaces/i-blog-post';
 import BlogPostPreview from '../../components/BlogPostPreview';
+import Head from 'next/head';
 
 type BlogProps = {
   blogPosts: IBlogPost[];
@@ -13,6 +14,21 @@ const Blog: NextPage<BlogProps> = (props) => {
 
   return (
     <div>
+      <Head>
+        <title>Blog - Matt Joll</title>
+        <meta
+          name="description"
+          content="Matt Joll is a Software Engineer from New Zealand with a passion for the web."
+        />
+        <meta name="og:title" content="Matt Joll" />
+        <meta
+          name="og:description"
+          content="Matt Joll is a Software Engineer from New Zealand with a passion for the web."
+        />
+        <meta name="og:url" content="https://mattjoll.nz" />
+        <meta name="og:type" content="website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <h1>Blog</h1>
       <div className="grid auto-rows-auto gap-4 grid-cols-1 lg:grid-cols-2 mt-5">
         {blogPostElements}
