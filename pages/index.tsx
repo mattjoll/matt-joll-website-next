5a27f824364c4207e8871cc84ea8aed7722bb9a1
Matthew Joll <dev@mattjoll.nz>
2021-09-27 13:27:56 +1300

    feat(seo): Add meta tags for the landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,27 @@
 import type { NextPage } from 'next';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
-  return <div>Coming soon!</div>;
+  return (
+    <div>
+      <Head>
+        <title>Matt Joll</title>
+        <meta
+          name="description"
+          content="Matt Joll is a Software Engineer from New Zealand with a passion for the web."
+        />
+        <meta name="og:title" content="Matt Joll" />
+        <meta
+          name="og:description"
+          content="Matt Joll is a Software Engineer from New Zealand with a passion for the web."
+        />
+        <meta name="og:url" content="https://mattjoll.nz" />
+        <meta name="og:type" content="website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h3>Coming soon!</h3>
+    </div>
+  );
 };
 
 export default Home;
