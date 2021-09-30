96ffdc86eaa6e38537d19ebf865d631c28dca39b
Matthew Joll <dev@mattjoll.nz>
2021-10-01 00:04:10 +1300

    feat(landing): Add about me section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from 'next';
+import Image from 'next/image';
 import PageHead from '../components/PageHead';
 
 const Home: NextPage = () => {
   return (
     <div>
       <PageHead />
-      <section className="mb-20 mt-12">
+      <section className="mb-20">
         <h1 className="max-w-sm">
           Kia ora, my name is <span className="text-green-400">Matt</span>
         </h1>
@@ -16,16 +17,42 @@ const Home: NextPage = () => {
 
       <section className="my-20">
         <h2>About me</h2>
-        <p>
-          This is some content. Quite a lot of content actually. I want to write
-          a lot of content so that I can test if my paragraphs look nice or not.
-        </p>
-        <p>
-          Now for some more content. Yay for content. In hindsight I couldve
-          used a lorem ipsum generator. Better yet, I could&apos;ve used bacon
-          ipsum. Why didn&apos;t I, you ask? Well, because I like to make my
-          life difficult when testing paragraphs and content.
-        </p>
+        <article className="flex flex-col items-center lg:flex-row">
+          <div>
+            <p>
+              As a developer, I want to help people. From teaching someone
+              who&apos;s never coded before to helping a large team write
+              software to stop bad guys, this is my drive and why I love doing
+              what I do.
+            </p>
+            <p>
+              I graduated from the University of Canterbury with a BSc in
+              Computer Science. During this time I managed to get some
+              experience as an intern while studying for my final year.
+            </p>
+            <p>
+              I currently work full time as a frontend software engineer, and
+              have experience in the full stack. I also enjoy taking on
+              contracting work as it gives me the opportunity to develop
+              different skills as every project is different!
+            </p>
+            <p>
+              When I&apos;m not coding, you can find me writing music, cooking,
+              or playing video games. Theres nothing quite like listening to
+              rock music and playing Final Fantasy while waiting for bread to
+              bake.
+            </p>
+          </div>
+          <div className="block flex-shrink-0 lg:ml-6">
+            <Image
+              alt="Matt Joll"
+              className="rounded-full"
+              src="/images/Matt-head.jpg"
+              height={300}
+              width={300}
+            />
+          </div>
+        </article>
       </section>
 
       <section className="my-20">
