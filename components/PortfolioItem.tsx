da40b0e4784cc4afd4ca931f489cc47ed8afafc2
Matthew Joll <dev@mattjoll.nz>
2021-10-29 23:39:51 +1300

    feat(portfolio): Add styling for link buttons when there isnt a link

--- components/PortfolioItem.tsx
+++ components/PortfolioItem.tsx
@@ -7,15 +7,33 @@ type PortfolioItemProps = {
   portfolioItem: IPortfolioItem;
 };
 
-const PortfolioItem: FunctionComponent<PortfolioItemProps> = (props) => {
+const PortfolioItem: FunctionComponent<PortfolioItemProps> = ({
+  portfolioItem,
+}: PortfolioItemProps) => {
+  const liveLinkClasses = !portfolioItem.repoUrl
+    ? 'cursor-not-allowed text-gray-400'
+    : 'cursor-pointer text-green-600';
+
+  const sourceLinkClasses = !portfolioItem.repoUrl
+    ? 'cursor-not-allowed text-gray-400'
+    : 'cursor-pointer text-green-600';
+
   return (
     <article className="flex flex-col justify-between p-4 rounded-lg shadow-lg lg:flex-row">
       <div className="flex flex-col">
-        <h2>{props.portfolioItem.title}</h2>
-        <p>{props.portfolioItem.description}</p>
+        <h2>{portfolioItem.title}</h2>
+        <p>{portfolioItem.description}</p>
         <a
-          className="cursor-pointer self-end mt-2 text-green-600"
-          href={props.portfolioItem.repoUrl}
+          className={`self-end mt-2 ${liveLinkClasses}`}
+          href={portfolioItem.repoUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View live app
+        </a>
+        <a
+          className={`self-end mt-2 ${sourceLinkClasses}`}
+          href={portfolioItem.repoUrl}
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -25,7 +43,7 @@ const PortfolioItem: FunctionComponent<PortfolioItemProps> = (props) => {
       <figure className="flex-shrink-0 mt-2 self-center lg:ml-6 lg:mt-0">
         <Image
           alt="Portfolio Item"
-          src={props.portfolioItem.imageUrl}
+          src={portfolioItem.imageUrl}
           height={200}
           width={360}
         />
