037302248ea7f9277d08814917c1b94a78c148aa
Matt Joll <dev@mattjoll.nz>
2021-10-30 00:17:23 +1300

    feat(portfolio): Add a link to view a live version of a portfolio item

--- components/PortfolioItem.tsx
+++ components/PortfolioItem.tsx
@@ -1,31 +1,29 @@
 import { FunctionComponent } from 'react';
 import Image from 'next/image';
-import DevIcon from './DevIcon';
 import { IPortfolioItem } from '../interfaces/i-portfolio-item';
+import LinkButton from './LinkButton';
 
 type PortfolioItemProps = {
   portfolioItem: IPortfolioItem;
 };
 
-const PortfolioItem: FunctionComponent<PortfolioItemProps> = (props) => {
+const PortfolioItem: FunctionComponent<PortfolioItemProps> = ({
+  portfolioItem,
+}: PortfolioItemProps) => {
   return (
     <article className="flex flex-col justify-between p-4 rounded-lg shadow-lg lg:flex-row">
       <div className="flex flex-col">
-        <h2>{props.portfolioItem.title}</h2>
-        <p>{props.portfolioItem.description}</p>
-        <a
-          className="cursor-pointer self-end mt-2 text-green-600"
-          href={props.portfolioItem.repoUrl}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          View source
-        </a>
+        <h2>{portfolioItem.title}</h2>
+        <p>{portfolioItem.description}</p>
+        <div className="flex mb-6 ml-auto gap-x-4">
+          <LinkButton url={portfolioItem.liveUrl}>View live example</LinkButton>
+          <LinkButton url={portfolioItem.repoUrl}>View source</LinkButton>
+        </div>
       </div>
       <figure className="flex-shrink-0 mt-2 self-center lg:ml-6 lg:mt-0">
         <Image
           alt="Portfolio Item"
-          src={props.portfolioItem.imageUrl}
+          src={portfolioItem.imageUrl}
           height={200}
           width={360}
         />
--- interfaces/i-portfolio-item.ts
+++ interfaces/i-portfolio-item.ts
@@ -2,6 +2,7 @@ export interface IPortfolioItem {
   description: string;
   id: string;
   imageUrl: string;
+  liveUrl: string;
   repoUrl: string;
   title: string;
 }
